cde5bfb6b803e7d5059be658f2aa5e2a81454d03
Rishabh Gupta <rishabh9aug@gmail.com>
2024-05-11 14:45:49 +0530

    giving time out but build is OK ,check AWS side

--- src/main/java/io/aws/spring/mysql/MainController.java
+++ src/main/java/io/aws/spring/mysql/MainController.java
@@ -1,6 +1,36 @@
 package io.aws.spring.mysql;
 
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+
+
+@RequestMapping(path = "/demo")
 public class MainController {
+
+    @Autowired
+    private userRepository UserRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody user addNewUser(@RequestParam String name,@RequestParam String email)
+    {
+        user springUser=new user();
+        springUser.setEmail(email);
+        springUser.setName(name);
+        UserRepository.save(springUser);
+        return springUser;
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<user> getAllUsers()
+    {
+        return UserRepository.findAll();
+    }
+
 
 
 }
